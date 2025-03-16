e2ae1104cfc5a92e1de1d6fad9ed72931bade471
wyo <kaywyo@gmail.com>
2025-03-16 20:33:08 +0200

    Add:icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -7,31 +7,31 @@ const SocialIcons = () => {
     <div className="fixed right-16 md:right-24 lg:right-32 top-1/2 -translate-y-1/2 flex flex-col gap-4 z-50">
       <Link
         to="https://www.facebook.com/NukecreativeSpace" target="_blank" rel="noopener noreferrer"
-        className="p-2 bg-[#FFD700] hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
-      > <Facebook className="h-7 w-7 text-sky-500 hover:text-sky-400 cursor-pointer transition-colors" />
+        className="p-2 bg-sky-500 hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
+      > <Facebook className="h-7 w-7 text-white hover:text-sky-400 cursor-pointer transition-colors" />
       </Link>
      
       <Link
         to="https://www.linkedin.com/company/4-pencils-animation"
         target="_blank"
         rel="noopener noreferrer"
-        className="p-2 bg-[#FFD700] hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
+        className="p-2 bg-sky-500 hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
       >
-        <LinkedinIcon className="h-7 w-7 text-sky-500 hover:text-sky-400 cursor-pointer transition-colors" />
+        <LinkedinIcon className="h-7 w-7 text-white hover:text-sky-400 cursor-pointer transition-colors" />
       </Link>
       <Link
         to="https://www.instagram.com/4_pencils_animations?igsh=a3UwamkzaW9peTRk&utm_source=qr"
         target="_blank"
         rel="noopener noreferrer"
-        className="p-2 bg-[#FFD700] hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
+        className="p-2 bg-sky-500 hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
       >
-         <Instagram className="h-7 w-7 text-sky-500 hover:text-sky-400 cursor-pointer transition-colors" />
+         <Instagram className="h-7 w-7 text-white hover:text-sky-400 cursor-pointer transition-colors" />
       </Link>
       <Link
         to="https://www.youtube.com/@4pencilsAnimations" 
-        className="p-2 bg-[#FFD700] hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
+        className="p-2 bg-sky-500 hover:bg-white/20 backdrop-blur-sm rounded-full transition-colors duration-300"
       >
-        <Youtube className="h-7 w-7 text-sky-500 hover:text-sky-400 cursor-pointer transition-colors" />
+        <Youtube className="h-7 w-7 text-white hover:text-sky-400 cursor-pointer transition-colors" />
       </Link>
     </div>
   );
